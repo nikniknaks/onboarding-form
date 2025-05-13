651f6752f61548626cb928ddabe126321fd88149
Nick Adams <6041525+nikniknaks@users.noreply.github.com>
2025-05-13 15:05:35 -0400

    remove boilerplate test

--- src/__tests__/hello.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Hello World Tests', () => {
-    it('should return true for true', () => {
-        expect(true).toBe(true);
-    });
-});
