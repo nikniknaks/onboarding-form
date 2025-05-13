0bab89eecd2dd345f92e9262cf258bf4b97c7a3c
Nick Adams <6041525+nikniknaks@users.noreply.github.com>
2025-05-13 15:52:57 -0400

    add github primer component library and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@primer/primitives/dist/css/functional/themes/light.css'
+import {BaseStyles, ThemeProvider} from '@primer/react'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <BaseStyles>
+        <App />
+      </BaseStyles>
+    </ThemeProvider>
   </StrictMode>,
 )
