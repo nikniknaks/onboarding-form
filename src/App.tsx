8b61d04123bf60dd33dccc5743a51d5990435b7e
Nick Adams <6041525+nikniknaks@users.noreply.github.com>
2025-05-13 20:59:39 -0400

    remove primer and add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import { Button, Heading, FormControl, TextInput, Box, PageLayout } from '@primer/react'
 import { useForm, SubmitHandler } from "react-hook-form"
 import React from 'react'
-import { error } from 'console'
+import { validateCorporationNumber } from './infrastructure/validateCorporationNumber'
 
 type Inputs = {
   firstName: string;
@@ -15,7 +13,6 @@ function App() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>({
     mode: 'onBlur',
@@ -24,59 +21,44 @@ function App() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
   const onBlurValidateFormField = (value) => {
-    console.log('Validating Corporation Number: ', value)
-    if (value && value.length < 5) {
-      console.log('Corporation Number Invalid')
-      return false
-    } else {
-      return true
+    if (!value) {
+      return false;
     }
+
+    return validateCorporationNumber(value).then((response) => {
+      return response.valid;
+    }).catch(() => {
+      return false;
+    });
   }
-
-  const [count, setCount] = useState(0)
-  // const handleSubmit = () => {
-  //   console.log('Form submitted')
-
-  // }
 
   return (
     <>
-    <Box  sx={{
-    backgroundColor: 'var(--bgColor-inset)',
-    borderRadius: 'var(--borderRadius-medium)',
-    padding: 'var(--stack-padding-spacious)',
-    fontFamily: 'var(--fontStack-sansSerif)',
-  }}>
-      <PageLayout padding='normal' rowGap='normal'>
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <PageLayout.Header>Onboarding Form</PageLayout.Header>
-        <PageLayout.Content>
-          <FormControl>
-            <FormControl.Label>First Name</FormControl.Label>
-            <TextInput size='large' {...register("firstName", { required: true, maxLength: 50, pattern: /^[A-Za-z]+$/i })} />
-            { errors.firstName && <FormControl.Validation variant="error">Invalid First Name. Max 50 Alphanumeric Characters</FormControl.Validation>}
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Last Name</FormControl.Label>
-            <TextInput size='large' {...register("lastName", { required: true, maxLength: 50, pattern: /^[A-Za-z]+$/i })}/>
-            { errors.lastName && <FormControl.Validation variant="error">Invalid Last Name. Max 50 Alphanumeric Characters</FormControl.Validation> }
-          </FormControl>
-        </PageLayout.Content>
-        <PageLayout.Content>
+    <div className={'bg-neutral-300'} style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}} >
+        <form onSubmit={handleSubmit(onSubmit)} style={
+          { width: '50%', display: 'flex', flexDirection: 'column', 
+            gap: '1rem', backgroundColor: 'white', borderRadius: '8px', borderStyle: 'solid', borderWidth: '2px', borderColor: '#EFEFEF', padding: '2rem',
+            margin: '2rem'}
+        }>
+        <div style={{display:"flex", justifyContent: 'center'}}>
+          <h1 style={{margin: '0px'}}>Onboarding Form</h1>
+        </div>
+            <label>First Name</label>
+            <input type="text" {...register("firstName", { required: true, maxLength: 50, pattern: /^[A-Za-z]+$/i })} />
+            { errors.firstName && <span>Invalid First Name. Max 50 Alphanumeric Characters</span>}
+         
+            <label>Last Name</label>
+            <input type="text" {...register("lastName", { required: true, maxLength: 50, pattern: /^[A-Za-z]+$/i })}/>
+            { errors.lastName && <span>Invalid Last Name. Max 50 Alphanumeric Characters</span> }
           
-        </PageLayout.Content>
-        <PageLayout.Content>
-          <FormControl>
-            <FormControl.Label>Phone Number</FormControl.Label>
-            <TextInput size='large' {...register("phone", { required: true, pattern: /^(\+?1[-.\s]?)?(\(?\d{3}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}$/i })}/>
-            { errors.phone && <FormControl.Validation variant="error">Invalid Phone Number. Valid Canadian Phone Number Required</FormControl.Validation> }
-          </FormControl>
-        </PageLayout.Content>
-        <PageLayout.Content>
-          <FormControl>
-            <FormControl.Label>Corporation Number</FormControl.Label>
-            <TextInput
-              size='large'
+            <label>Phone Number</label>
+            <input type="text" {...register("phone", { required: true, pattern: /^(\+?1[-.\s]?)?(\(?\d{3}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}$/i })}/>
+            { errors.phone && <span>Invalid Phone Number. Valid Canadian Phone Number Required</span> }
+          
+            <label>Corporation Number</label>
+            <input
+              type="text"
+           
               {
                 ...register(
                   "corporationNumber",
@@ -87,15 +69,10 @@ function App() {
                 )
               }
             />
-            { errors.corporationNumber && <FormControl.Validation variant="error">Invalid Corporation Number. Max 50 Alphanumeric Characters</FormControl.Validation> }
-          </FormControl>
-        </PageLayout.Content>
-        <PageLayout.Footer>
-          <Button onClick={() => handleSubmit()}>Submit</Button>
-        </PageLayout.Footer>
+            { errors.corporationNumber && <span>Invalid Corporation Number. Max 50 Alphanumeric Characters</span> }
+          <button type='submit' style={{backgroundColor: 'black'}}>Submit</button>
         </form>
-      </PageLayout>
-      </Box>
+        </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@primer/primitives/dist/css/functional/themes/light.css'
-import {BaseStyles, ThemeProvider} from '@primer/react'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <BaseStyles>
         <App />
-      </BaseStyles>
-    </ThemeProvider>
   </StrictMode>,
 )
